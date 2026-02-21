8011e94d1b9667fdaea59359d3e913a6795298aa
wooskor79 <newkids79@gmail.com>
2026-02-21 20:11:33 +0900

    update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/phpmyadmin': {
+        target: 'http://phpmyadmin:80',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/phpmyadmin/, '')
+      }
+    }
+  }
 })
